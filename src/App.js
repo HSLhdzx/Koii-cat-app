eceb5f15dbc87274d232c5dc637230a74a233a95
LH <>
2024-04-21 02:53:38 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './components/style.css';
 
 function App() {
   return (
-    <div className="App m-2 max-w-full text-center px-52" id='mainss'>
+    <div className="App m-2 max-w-full text-center px-52" id='main'>
         <Board></Board>
     </div>
   );
